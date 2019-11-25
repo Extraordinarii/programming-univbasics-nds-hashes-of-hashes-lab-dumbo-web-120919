5e8112d792c7da772807336d77df8bff2efcc04e
Alisher Fayzimatov <alisherfayzimatov@gmail.com>
2019-11-25 19:11:34 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -31,11 +31,23 @@
 # }
 
 def naming_system
- { label: Kingdom
+ { label: "Kingdom"
     sub_category: {
       label: "Phylum",
       sub_category: {
-        label: "Class"
+        label: "Class",
+        sub_category: {
+          label: "Order",
+          sub_category: {
+            label: "Family",
+            sub_category: {
+              label: "Genus",
+              sub_category: {
+                label: "Species"
+              }
+            }
+          }
+        }
       }
       
     }
